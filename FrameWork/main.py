8061b0882676a703e891aece9b750e24aa73f3c5
jameskrw <krwang2022@gmail.com>
2023-03-08 10:40:18 +0000

    test results

--- FrameWork/main.py
+++ FrameWork/main.py
@@ -22,7 +22,7 @@ def main(args):
         data_stream = pickle.load(f)
     
     # data_stream=data[1:,-2]
-    dd=DriftDetection(data_stream)
+    dd=DriftDetection(ogdata)
     dd.add_method(ADWIN(),1)
     dd.add_method(DDM(),1)
     dd.add_method(EDDM(),1)
@@ -30,15 +30,22 @@ def main(args):
     dd.add_method(HDDM_W(),1)
     dd.add_method(PageHinkley(),1)
     dd.add_method(KSWIN(),1)
-    window_size=50
-    thresh_hold=10
+    window_size=75
+    thresh_hold=300
     change_list=dd.get_voted_drift(window_size=window_size,thresh_hold=thresh_hold)
+    change_list=[i+1000 for i in change_list]
     rec_time=int(time.time())
-    plot(ogdata,change_list,f'{args.data_path}/mlp_{rec_time}_{window_size}_{thresh_hold}.png')
+    plot(ogdata,change_list,f'{args.data_path}/og_{rec_time}_{window_size}_{thresh_hold}.png')
 
 if __name__=='__main__':
+    og_path_list=["m_1.csv","m_14.csv","m_32.csv","m_42.csv","m_55.csv","m_98.csv","m_155.csv","m_259.csv","m_350.csv","m_450.csv"]
+
+    data_path_list=["m_1/2023-03-08T03-59-13","m_14/2023-03-08T03-59-24","m_32/2023-03-08T03-59-31",
+                    "m_42/2023-03-08T03-59-36","m_55/2023-03-08T03-59-42","m_98/2023-03-08T03-59-47",
+                    "m_155/2023-03-08T03-59-52","m_259/2023-03-08T04-00-03","m_350/2023-03-08T04-00-10","m_450/2023-03-08T04-00-16"]
+    i=9
     parser = argparse.ArgumentParser(description='training template')
-    parser.add_argument('--og_path', type=str, default='/home/cc/github/VotingBasedDriftDetection/methods/data/m_1.csv', metavar='N',)
-    parser.add_argument('--data_path', type=str, default='/home/cc/github/VotingBasedDriftDetection/m_1/2023-03-08T03-59-13', metavar='N',)
+    parser.add_argument('--og_path', type=str, default=f'/home/cc/github/VotingBasedDriftDetection/methods/data/{og_path_list[i]}', metavar='N',)
+    parser.add_argument('--data_path', type=str, default=f'/home/cc/github/VotingBasedDriftDetection/{data_path_list[i]}', metavar='N',)
     args = parser.parse_args()
     main(args)
